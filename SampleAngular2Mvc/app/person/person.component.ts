f59f4dbb5ccb5d6c29159dcbdb4a98ae9d8111ed
Michael Lang <michael.lang@nexul.com>
2017-03-03 15:04:42 -0600

    Person orchestrator added and used throughout as appropriate.  App works as it did prior to this commit.

--- SampleAngular2Mvc/app/person/person.component.ts
+++ SampleAngular2Mvc/app/person/person.component.ts
@@ -1,12 +1,8 @@
 ﻿import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import 'rxjs/rx';
-import { Store } from '@ngrx/store';
 import { Tab } from '../app-shared/tabset/tab.model';
 import { Person, PersonHolder } from './person.model';
-import { PersonService } from './person.service';
-import { AppState } from '../app.store';
-import { PersonCloseAction, PersonOpenAction, PersonTabActivateAction } from './person.store';
+import { PersonOrchestratorService } from './person-orchestrator.service';
 
 @Component({
     selector: 'app-person',
@@ -16,36 +12,11 @@ export class PersonComponent {
     openItems$: Observable<PersonHolder[]>;
     tabs$: Observable<Tab[]>;
     activeTabId$: Observable<string>;
-    searchTab: Tab;
 
-    constructor(private _store: Store<AppState>, private _itemService: PersonService) {
-        this.searchTab = new Tab();
-        this.searchTab.id = 'tab-id-search';
-        this.searchTab.title = 'Search';
-        this.searchTab.template = 'search';
-        this.searchTab.active = true;
-        this.searchTab.closeable = false;
-        this.searchTab.itemId = '';
-
-        this.activeTabId$ = _store.select(x => x.person.activeTabId);
-        this.openItems$ = _store.select(x => x.person.openList);
-        this.tabs$ = this.openItems$
-            .combineLatest(this.activeTabId$)
-            .map(([people, activeId]) => {
-                this.searchTab.active = activeId === this.searchTab.id || !activeId;
-                return [this.searchTab].concat(people.map(item => {
-                    let exists = item.Person && item.Person.PersonId && item.Person.PersonId.length > 0;
-                    let t = new Tab();
-                    t.id = item.PlaceholderId;
-                    t.title = exists ? item.Person.LastName + ' ' + item.Person.FirstName : 'Add Person';
-                    t.template = 'item';
-                    t.active = activeId === t.id;
-                    t.closeable = true;
-                    t.item = item;
-                    t.itemId = item.Person.PersonId;
-                    return t;
-                }));
-            });
+    constructor(private _itemService: PersonOrchestratorService) {
+        this.activeTabId$ = _itemService.activeTabId;
+        this.openItems$ = _itemService.openList;
+        this.tabs$ = _itemService.tabs;
     }
 
     savePerson(vh: PersonHolder) {
@@ -53,15 +24,15 @@ export class PersonComponent {
     }
 
     closeTab(tab: Tab) {
-        this._store.dispatch(new PersonCloseAction(tab.id))
+        this._itemService.close(tab.id);
     }
     addTab(b: Boolean) {
-        this._store.dispatch(new PersonOpenAction(new Person()));
+        this._itemService.add();
     }
     activateTab(id: string) {
-        this._store.dispatch(new PersonTabActivateAction(id));
+        this._itemService.activate(id);
     }
     openItem(person: Person) {
-        this._store.dispatch(new PersonOpenAction(person));
+        this._itemService.open(person);
     }
 }
--- SampleAngular2Mvc/app/person/person.service.ts
+++ SampleAngular2Mvc/app/person/person.service.ts
@@ -1,57 +1,16 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
-import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
 import { Configuration } from '../app.constants';
-import {
-    Person,
-    PersonHolder,
-    PersonCriteria
-} from './person.model';
-import { AppState } from '../app.store';
-import {
-    PersonSearchAction,
-    PersonSearchCompleteAction,
-    PersonUpdateCompleteAction,
-    PersonInsertCompleteAction
-} from './person.store';
+import { GenericService } from '../app-shared/generic.service'
+import { Person, PersonCriteria } from './person.model';
 
 @Injectable()
-export class PersonService {
-    private baseUrl: string;
-    private headers: Headers;
-
-    constructor(private _http: Http, private _configuration: Configuration, private _store: Store<AppState>) {
-        this.baseUrl = _configuration.Server + _configuration.ApiUrl + 'person';
-        this.headers = new Headers();
-        this.headers.append('Content-Type', 'application/json');
-        this.headers.append('Accept', 'application/json');       
+export class PersonService extends GenericService<PersonCriteria, Person> {
+    constructor(_http: Http, _configuration: Configuration) {
+        super(_http, _configuration)
     }
-
-    public search(criteria: PersonCriteria) {
-        this._store.dispatch(new PersonSearchAction(criteria));
-        this._http.post(this.baseUrl + '/search', criteria, { headers: this.headers })
-            .map((response: Response) =>
-                <Person[]>response.json())
-            .subscribe(payload =>
-                this._store.dispatch(new PersonSearchCompleteAction(payload)),
-                error => { }, //TODO: call another search failed action??
-                () => { } //on complete
-            );
-    }
-    public save(item: PersonHolder) {
-        let isUpdate = item.Person.PersonId;
-        return this._http.post(this.baseUrl + '/', item.Person, { headers: this.headers })
-            .map((response: Response) => <Person>response.json())
-            .subscribe(result => {
-                let updated = new PersonHolder();
-                updated.PlaceholderId = item.PlaceholderId;
-                updated.Person = result;
-                isUpdate
-                    ? this._store.dispatch(new PersonUpdateCompleteAction(updated))
-                    : this._store.dispatch(new PersonInsertCompleteAction(updated))
-                },
-                error => { }, //TODO: call another save failed action??
-                () => { } //on complete
-            );
+    get endpoint(): string {
+        return 'person';
     }
 }
