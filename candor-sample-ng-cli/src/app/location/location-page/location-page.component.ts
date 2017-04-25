cbff0e8a3d1356a24abc17edab58130613e05fd1
Michael Lang <michael.lang@nexul.com>
2017-04-25 12:51:06 -0500

    Added location module and component
    CLI commands only
    ng g module location --flat false --routing
    ng g component location-page

---
+++ candor-sample-ng-cli/src/app/location/location-page/location-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location-page',
+  templateUrl: './location-page.component.html',
+  styleUrls: ['./location-page.component.scss']
+})
+export class LocationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
